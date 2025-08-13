8392eba4ee4c344ae4c45d3afbdd9b8bb0a044db
PP-BIN <pixcelpuzzel@gmail.com>
2025-08-13 19:17:56 +0900

    refactor: share normalize utility

--- src/utils/slugify.js
+++ src/utils/slugify.js
@@ -1,10 +1,12 @@
-export default function slugify(text = "") {
-  return text
-    .toString()
+export const normalize = (s = "") =>
+  String(s)
     .normalize("NFKD")
-    .replace(/[\u0300-\u036f]/g, "") // accents
-    .replace(/[^\w\s-]/g, "")        // non-word
+    .replace(/[\u0300-\u036f]/g, "")
+    .replace(/[^\w\s.\-가-힣]/g, "")
     .trim()
     .replace(/\s+/g, "-")
     .toLowerCase();
+
+export default function slugify(text = "") {
+  return normalize(text);
 }
